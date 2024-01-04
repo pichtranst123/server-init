ea6eb5779c6067f7616db020788fee9f6e5f46c2
Alexandros De Great <126134422+pichtranst123@users.noreply.github.com>
2024-01-04 22:44:14 +0700

    Update typescript serve & change ->ts.config.json!

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,11 +1,11 @@
-import http from 'http';
+import * as http from 'http';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+  res.end('Hello World\n');
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}/`);
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
